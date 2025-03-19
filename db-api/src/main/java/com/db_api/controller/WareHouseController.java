825014fbf71afcaa7e1819694c17257b9146ebff
MushfiqueAlam <mushfique.037@gmail.com>
2025-03-20 01:06:25 +0530

    add some crud operation on warehouse

--- db-api/src/main/java/com/db_api/controller/WareHouseController.java
+++ db-api/src/main/java/com/db_api/controller/WareHouseController.java
@@ -2,10 +2,9 @@ package com.db_api.controller;
 
 import com.db_api.models.WareHouse;
 import com.db_api.repository.WareHoseRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/db/warehouse")
@@ -15,9 +14,21 @@ public class WareHouseController {
     public WareHouseController(WareHoseRepository wareHoseRepository){
         this.wareHoseRepository=wareHoseRepository;
     }
+
     @PostMapping("/save")
     public WareHouse createWareHouse(@RequestBody WareHouse wareHouse){
         WareHouse wareHouse1=wareHoseRepository.save(wareHouse);
         return wareHouse1;
+    }
+
+    @GetMapping("/{wareHouseId}")
+    public WareHouse getWareHouseById(@PathVariable UUID wareHouseId){
+        return wareHoseRepository.findById(wareHouseId).orElse(null);
+    }
+
+    @PutMapping("/update")
+    public WareHouse updateWareHouse(@RequestBody WareHouse wareHouse){
+       wareHoseRepository.save(wareHouse);
+       return wareHouse;
     }
 }
