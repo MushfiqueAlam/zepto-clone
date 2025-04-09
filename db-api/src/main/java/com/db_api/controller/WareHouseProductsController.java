61d1fbae3c878b2e5542c4af8362d0ff020c3d0d
MushfiqueAlam <mushfique.037@gmail.com>
2025-04-10 01:08:30 +0530

    add order logic in db api

--- db-api/src/main/java/com/db_api/controller/WareHouseProductsController.java
+++ db-api/src/main/java/com/db_api/controller/WareHouseProductsController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/db/warehouse/products")
+@RequestMapping("/api/v1/db/warehouse/product")
 public class WareHouseProductsController {
 
     @Autowired
